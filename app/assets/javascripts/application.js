a2462e84f9bba5572aa86cdd5f030cde30008868
Christopher Diaz <crdiaz3@asu.edu>
2016-02-23 09:52:17 -0700

    Test Alert

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,20 +16,6 @@
 //= require_tree .
 
 // FLASH NOTICE ANIMATION
-var fade_flash = function() {
-    $("#flash_notice").delay(5000).fadeOut("slow");
-    $("#flash_alert").delay(5000).fadeOut("slow");
-    $("#flash_error").delay(5000).fadeOut("slow");
-};
-fade_flash();
-
-var show_ajax_message = function(msg, type) {
-    $("#flash-message").html('<div id="flash_'+type+'">'+msg+'</div>');
-    fade_flash();
-};
-
-$(document).ajaxComplete(function(event, request) {
-    var msg = request.getResponseHeader('X-Message');
-    var type = request.getResponseHeader('X-Message-Type');
-    show_ajax_message(msg, type); //use whatever popup, notification or whatever plugin you want
-});
+$('#notice').html("<%= escape_javascript raw(flash_notice) %>");
+<% flash.discard %>
+$("[id^=flash_]").show().delay(2000).fadeOut(1000)
