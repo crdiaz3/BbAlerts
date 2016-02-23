59fd2af5c87249c02c3d2bf66fe2f2118554c717
Christopher Diaz <crdiaz3@asu.edu>
2016-02-23 09:20:02 -0700

    Test Alert

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,13 @@
+def show_flash(options = {})
+    output = ActiveSupport::SafeBuffer.new
+
+    [:alert, :notice].each do |message|
+        output << content_tag(:p, class: [message, options[:class]], tabindex: '0') do
+            flash[message]
+        end if flash[message].present?
+
+        flash[message] = nil
+     end
+
+     output
+end
