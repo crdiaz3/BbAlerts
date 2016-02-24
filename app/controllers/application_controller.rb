cd2e36e04c0b1a76675dadb5d8a8cfeb939daa79
Christopher Diaz <crdiaz3@asu.edu>
2016-02-24 15:11:51 -0700

    Restrict access by ip - Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :block_foreign_hosts
+
+  def whitelisted?(ip)
+    return true if [192.168.0.1].include?(ip)
+    false
+  end
+
+  def block_foreign_hosts
+    return false if whitelisted?(request.remote_ip)
+    redirect_to "https://www.google.com" unless request.remote_ip.start_with?("192.168.0")
+  end
 
 end
