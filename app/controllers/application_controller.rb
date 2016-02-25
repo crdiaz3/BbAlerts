f831dab4fa7a9f5ac45aef60b0541136102247a8
Christopher Diaz <crdiaz3@asu.edu>
2016-02-24 22:56:03 -0700

    Restrict access by ip - Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  WHITELIST = ["127.0.0.1","192.168.0.1","192.168.0.3"]
+  WHITELIST = ["97.124.120.200"]
   before_filter :check_whitelist
 
   def check_whitelist
