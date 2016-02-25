b08f7d7b66474784e8a2472ee55302c60f678b89
Christopher Diaz <crdiaz3@asu.edu>
2016-02-24 22:43:51 -0700

    Restrict access by ip - Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  WHITELIST = ["127.0.0.1","192.168.0.1","192.168.0.3"]
+  before_filter :check_whitelist
 
-  before_filter :block_ip
+  def index
+    render(:text => "You are not authorized to view this page.")
+  end
 
-  def block_ip
-    if request.remote_ip.match('127.0.0.*')
-      redirect_to "http://YourCanonicalDomain.tld/503.html"
-      return
+  def check_whitelist
+    if WHITELIST.include?(request.remote_ip)
+      true
+    else
+      render(:nothing => true, :status => 401)
+      false
     end
   end
 end
