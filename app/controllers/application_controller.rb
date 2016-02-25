7f90313c706d90aa5ffc240d85943de385faa62a
Christopher Diaz <crdiaz3@asu.edu>
2016-02-24 21:53:13 -0700

    Restrict access by ip - Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :protect
+  before_filter :block_ip
 
-  def protect
-    @ips = ['192.168.0.1', '192.168.0.3'] #And so on ...]
-    if not @ips.include? request.remote_ip
-       # Check for your subnet stuff here, for example
-       # if not request.remote_ip.include?('127.0,0')
-       render :text => "You are unauthorized"
-       return
+  def block_ip
+    if request.remote_ip.match('192.168.0.*')
+      redirect_to "http://YourCanonicalDomain.tld/503.html"
+      return
     end
   end
 end
