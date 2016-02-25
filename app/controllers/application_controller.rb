62d6caf43486b9f17cb917c7a67f0209de6beaa6
Christopher Diaz <crdiaz3@asu.edu>
2016-02-24 22:52:52 -0700

    Restrict access by ip - Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,11 @@ class ApplicationController < ActionController::Base
   WHITELIST = ["127.0.0.1","192.168.0.1","192.168.0.3"]
   before_filter :check_whitelist
 
-  def index
-    render(:text => "You are not authorized to view this page.")
-  end
-
   def check_whitelist
     if WHITELIST.include?(request.remote_ip)
       true
     else
-      render(:nothing => true, :status => 401)
+      render :text => "You are unauthorized"
       false
     end
   end
